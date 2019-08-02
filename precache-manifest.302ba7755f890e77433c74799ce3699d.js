f33f4ba412a5668ef797456c6d8ddd7846b2b5b0
André Lima <hello@andrelima.space>
2019-08-02 09:09:57 -0300

    Updates

---
+++ precache-manifest.302ba7755f890e77433c74799ce3699d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984d8173a571f4fcd9f55c08360a5d26",
+    "url": "/know-your-hero/index.html"
+  },
+  {
+    "revision": "2645843406955547c6a1",
+    "url": "/know-your-hero/static/css/main.ba2e6a5c.chunk.css"
+  },
+  {
+    "revision": "6f1643755f10c44269f5",
+    "url": "/know-your-hero/static/js/2.bb760fec.chunk.js"
+  },
+  {
+    "revision": "2645843406955547c6a1",
+    "url": "/know-your-hero/static/js/main.84c60288.chunk.js"
+  },
+  {
+    "revision": "25523fa68553668e6ef9",
+    "url": "/know-your-hero/static/js/runtime~main.10620f7e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/know-your-hero/precache-manifest.302ba7755f890e77433c74799ce3699d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/know-your-hero/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
